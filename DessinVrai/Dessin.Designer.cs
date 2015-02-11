2b818ba0c094c6d13c8bb97965a897daafa47bfe
quixo33 <m.renaudon@gmail.com>
2015-02-11 15:50:07 +0100

    Suppression Menu Tool

--- DessinVrai/Dessin.Designer.cs
+++ DessinVrai/Dessin.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dessin));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.deplacement = new System.Windows.Forms.ToolStripButton();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,7 +38,7 @@
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.deplacement});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(284, 25);
             this.toolStrip1.TabIndex = 0;
@@ -55,23 +54,13 @@
             this.deplacement.Size = new System.Drawing.Size(23, 22);
             this.deplacement.Text = "Déplacement";
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
             // Dessin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.menuStrip1);
             this.DoubleBuffered = true;
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Dessin";
             this.Text = "Dessin";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Dessin_Paint);
@@ -89,7 +78,6 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton deplacement;
-        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
